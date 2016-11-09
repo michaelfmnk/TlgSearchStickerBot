1daa80fe86bb01063b4d0b0f89060ac3f76c8409
michael <michael.fmnk@gmail.com>
2016-11-09 16:20:40 +0200

    new

--- settings.py
+++ settings.py
@@ -1,22 +1,3 @@
 key = "285624752:AAEcPjz0rPMGVowNil3fu4YvoBlxP_YROg4"
-
-data = {"туманно будущее этого мальчика": "BQADAgADRQADkRziCHmqQZECnfU5Ag",
-        "херня вышла": "BQADAgADRQADSnO6C1AmpW9olivFAg",
-        "я ленивая жопа": "BQADAgADQwIAAmy_FgXIPT17tHV7PgI",
-        "language": "BQADBAADrAIAAh4OmQMqEestG5BSbwI",
-        "а шо такое?": "BQADAgADMAEAAr8cUgEYtvDclUZ8pQI",
-        "spoiler _спойлер": "BQADAQADNwAD_2raAQkjabZbk5hPAg",
-        "spoiler __спойлер": "BQADAQADOQAD_2raAZ1YqIzMSauEAg",
-         "spoiler ___спойлер": "BQADAQADOwAD_2raAaDxSecI_1-0Ag",
-         "spoiler ____спойлер": "BQADAQADQAAD_2raAZ11d_TXFtyvAg",
-         "spoiler ______спойлер": "BQADAQADQgAD_2raAffGcRDfXn4qAg",
-         "spoiler _______спойлер": "BQADAQADRAAD_2raAUkUSR5fdPtEAg",
-         "spoiler ________спойлер": "BQADAQADRgAD_2raAVIM6-mGviR3Ag",
-         "spoiler _________спойлер": "BQADAQADSAAD_2raAZCuwpnx1qVEAg",
-         "spoiler __________спойлер": "BQADAQADSgAD_2raARzs_EKJlUdEAg",
-         "не бездельник а философ": "BQADAgADQwADSnO6C0j9pq7pIzLYAg"
-        }
-
 text_send_title="Пожалуйста, отправьте название по которому можно будет искать ваш стикер "
-
 debug = True
